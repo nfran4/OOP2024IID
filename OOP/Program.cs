a50a95814ba2d7c525b6cfaa3fbf9245615d7b8e
Nazar Franchuk <w69938@student.wsiz.edu.pl>
2024-10-17 11:23:51 +0200

    first commit

---
+++ OOP/Program.cs
@@ -0,0 +1,5 @@
+﻿using OOP.Constructions;
+Console.WriteLine("Hello, Word!");
+var construction1 = new Construction();
+var construction2 = new Construction(23.0f, 45.0f, 4, 4, "Material");
+Console.WriteLine("Hello, Word!");
